f08f05ddbbad5d253ed50e67597b40fb574668d1
Gaoyuan Zhang <zgy25483387@gmail.com>
2023-08-17 17:35:22 -0700

    subsetII

--- JavaSolution/src/SubsetII.java
+++ JavaSolution/src/SubsetII.java
@@ -16,21 +16,31 @@ public class SubsetII {
     }
 
     public static void dfs(Integer i, int[] nums, List<Integer> currSet, List<List<Integer>> resArr) {
-        resArr.add(new ArrayList<>(currSet));
-        for (int j = i; j < nums.length; j++) {
-            if (j != i && nums[j] == nums[j - 1]) {
-                continue;
+        if (i >= nums.length) {
+            if (!resArr.contains(currSet)) {
+                resArr.add(new ArrayList<>(currSet));
             }
-            currSet.add(nums[j]);
-            dfs(j + 1, nums, currSet, resArr);
-            currSet.remove(currSet.size() - 1);
-
+            return;
         }
+        // when yes,
+        // check if currSet exist in resArr
+        // if no, add currSet in resArr
+        // return
+
+        // recursive case
+        // add ith element in currSet
+        currSet.add(nums[i]);
+        // dfs(i + 1);
+        dfs(i + 1, nums, currSet, resArr);
+        // pop ith element in currSet
+        currSet.remove(currSet.size() - 1);
+        // dfs(i + 1)
+        dfs(i + 1, nums, currSet, resArr);
 
     }
 
     public static void main(String[] args) {
-        int[] nums = {4,1, 4};
+        int[] nums = {4,1,4};
         System.out.println(subsetsWithDup(nums));
     }
 }
