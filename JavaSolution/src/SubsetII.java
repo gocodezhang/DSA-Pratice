b7de39d2b36a70cda4f8fbb83c4b9cea8c96a596
Gaoyuan Zhang <zgy25483387@gmail.com>
2023-08-20 15:43:55 -0700

    alternative approach for subsetII

--- JavaSolution/src/SubsetII.java
+++ JavaSolution/src/SubsetII.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class SubsetII {
     public static List<List<Integer>> subsetsWithDup(int[] nums) {
@@ -39,8 +37,48 @@ public class SubsetII {
 
     }
 
+    public static List<List<Integer>> subsetsWithDup2(int[] nums) {
+        List<List<Integer>> resultArr = new ArrayList<>();
+        List<Integer> currSet = new ArrayList<>();
+        Map<Integer, Integer> counterMap = new HashMap<>();
+        for (int num : nums) {
+            if (counterMap.containsKey(num)) {
+                counterMap.put(num, counterMap.get(num) + 1);
+            } else {
+                counterMap.put(num, 1);
+            }
+        }
+        List<int[]> counterList = new ArrayList<>();
+        counterMap.forEach((c, f) -> {
+            counterList.add(new int[]{c, f});
+        });
+        dfs(currSet, resultArr, counterList, 0);
+
+        return resultArr;
+
+    }
+    public static void dfs(List<Integer> currSet, List<List<Integer>> resultArr, List<int[]> counter, int index) {
+        resultArr.add(new ArrayList<>(currSet));
+        for (int i = index; i < counter.size(); i++) {
+            int[] currTuple = counter.get(i);
+            int candidate = currTuple[0];
+            int frequency = currTuple[1];
+            if (frequency <= 0) {
+                continue;
+            }
+            currSet.add(candidate);
+            counter.set(i, new int[]{candidate, frequency - 1});
+            dfs(currSet, resultArr, counter, i);
+            currSet.remove(currSet.size() - 1);
+            counter.set(i, new int[]{candidate, frequency});
+
+        }
+
+    }
+
     public static void main(String[] args) {
         int[] nums = {4,1,4};
-        System.out.println(subsetsWithDup(nums));
+        // System.out.println(subsetsWithDup(nums));
+        System.out.println(subsetsWithDup2(nums));
     }
 }
