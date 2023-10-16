b32563b8d55d1e30934f8246675fd7ccd9b54ff8
Gaoyuan Zhang <zgy25483387@gmail.com>
2023-10-15 17:45:44 -0700

    boyer moore voting

--- JavaSolution/src/MajorityElement.java
+++ JavaSolution/src/MajorityElement.java
@@ -14,5 +14,21 @@ public class MajorityElement {
         }
         return -1;
     }
+    /**
+     * boyer-moore voting algorithm
+     * In case array (nums) doesn't guarantee to have a majority element
+     * we will need to traverse the array one more time to verify if the candidate is actually majority element
+     */
+    public static int majorityEleOptimal(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+        for (int i = 0; i < nums.length; i++) {
+            if (count == 0) {
+                candidate = nums[i];
+            }
+            count = (nums[i] == candidate) ? count + 1 : count - 1;
+        }
+        return candidate;
+    }
 
 }
