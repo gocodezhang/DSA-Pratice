a2ca22a606fa63315283ed76342286cecfafc953
Gaoyuan Zhang <zgy25483387@gmail.com>
2023-11-13 10:34:52 -0800

    iterative approach

--- JavaSolution/src/InvertBinaryTree.java
+++ JavaSolution/src/InvertBinaryTree.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class InvertBinaryTree {
     public static TreeNode invertTree(TreeNode root) {
         // when root == null
@@ -15,12 +18,33 @@ public class InvertBinaryTree {
         // return invertedRoot
         return invertedRoot;
     }
+    public static TreeNode invertTreeIterative(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (queue.size() > 0) {
+            TreeNode currNode = queue.poll();
+            TreeNode temp = currNode.left;
+            currNode.left = currNode.right;
+            currNode.right = temp;
+            if (currNode.left != null) {
+                queue.add(currNode.left);
+            }
+            if (currNode.right != null) {
+                queue.add(currNode.right);
+            }
+
+        }
+        return root;
+    }
     public static void main(String[] args) {
         TreeNode root = new TreeNode(4);
         TreeNode left = new TreeNode(3);
         TreeNode right = new TreeNode(2);
         root.left = left;
         root.right = right;
-        System.out.println(invertTree(root));
+        System.out.println(invertTreeIterative(root));
     }
 }
