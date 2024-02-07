faf09ff65434f607d72e4c070665cb246009d74c
Gaoyuan Zhang <zgy25483387@gmail.com>
2024-02-06 21:19:28 -0800

    linked list cycle

--- JavaSolution/src/LinkedListCycle.java
+++ JavaSolution/src/LinkedListCycle.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class LinkedListCycle {
+import java.util.List;
+
+public class LinkedListCycle {
+    public static boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode reg = head;
+        ListNode fast = head;
+        while (reg != null && fast != null) {
+            reg = reg.next;
+            fast = fast.next;
+            if (fast != null) {
+                fast = fast.next;
+            }
+            if (reg != null && fast != null && reg == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        ListNode node2 = new ListNode(2);
+        ListNode node0 = new ListNode(0);
+        ListNode node4 = new ListNode(4);
+        head.next = node2;
+        node2.next = node0;
+        node0.next = node4;
+//        node4.next = node2;
+        System.out.println(hasCycle(head));
+
+    }
 }
