f743851038ffe43c5a81cfa7c2cfb9f71be5346a
Gaoyuan Zhang <zgy25483387@gmail.com>
2024-08-04 19:33:48 -0700

    zigzag

--- JavaSolution/src/ZigZag.java
+++ JavaSolution/src/ZigZag.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class ZigZag {
+public class ZigZag {
+    public String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+        // define section length
+        int sessionLength = (2 * numRows) - 2;
+        StringBuilder builder = new StringBuilder();
+
+        for (int row = 0; row < numRows; row++) {
+            int startIndex = row;
+            while (startIndex < s.length()) {
+                builder.append(s.charAt(startIndex));
+                if (row != 0 & row != (numRows - 1)) {
+                    int increment = sessionLength - (2 * row);
+                    int secondIndex = startIndex + increment;
+                    if (secondIndex < s.length()) {
+                        builder.append(s.charAt(secondIndex));
+                    }
+                }
+                startIndex += sessionLength;
+            }
+        }
+        return builder.toString();
+    }
+    public static void main(String[] args) {
+        String s = "PAYPALISHIRING";
+        ZigZag zigzag = new ZigZag();
+        System.out.println(zigzag.convert(s, 4));
+    }
+
 }
