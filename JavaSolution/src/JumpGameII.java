da5bcf260126dc77028d5808468ffa224e0392a1
Gaoyuan Zhang <zgy25483387@gmail.com>
2024-11-16 11:02:17 -0800

    jump game II

--- JavaSolution/src/JumpGameII.java
+++ JavaSolution/src/JumpGameII.java
@@ -1,23 +1,92 @@
+import java.util.Arrays;
+
 public class JumpGameII {
-    public static int minJump(int[] nums) {
-        int steps = 0;
+    public int jumpWithDfs(int[] nums) {
+        int[] memo = new int[nums.length];
+        Arrays.fill(memo, -1);
+        return dfs(0, nums, memo);
+    }
+    private int dfs(int currPosition, int[] nums, int[] memo) {
+        // base case
+        if (currPosition == nums.length - 1) {
+            return 0;
+        }
+        if (memo[currPosition] != -1) {
+            return memo[currPosition];
+        }
+
+        // recursive case
+        int maxStep = Math.min(nums[currPosition], nums.length - 1 - currPosition);
+        int minStepToDst = Integer.MAX_VALUE;
+        for (int step = 1; step <= maxStep; step++) {
+            minStepToDst = Math.min(dfs(currPosition + step, nums, memo), minStepToDst);
+        }
+
+        int result = minStepToDst == Integer.MAX_VALUE ? minStepToDst : minStepToDst + 1;
+        memo[currPosition] = result;
+
+        return result;
+    }
+    public int jumpWithDp(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[nums.length - 1] = 0;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            int maxStep = Math.min(nums[i], nums.length - 1 - i);
+            int min = Integer.MAX_VALUE;
+            for (int step = 1; step <= maxStep; step++) {
+                min = Math.min(min, dp[i + step]);
+            }
+            if (min < Integer.MAX_VALUE) {
+                dp[i] = min + 1;
+            }
+        }
+
+        return dp[0];
+    }
+    public int jumpWithRange(int[] nums) {
+        // define a jump range
+        int jumpStep = 0;
         int left = 0;
         int right = 0;
-
+        // while the range cannot get to the dst
         while (right < nums.length - 1) {
-            int farthestPos = 0;
+            // go through curr range
+            int furthest = right + 1;
             for (int i = left; i <= right; i++) {
-                farthestPos = Math.max(farthestPos, i + nums[i]);
+                int rightMostIndex = Math.min(i + nums[i], nums.length - 1);
+                furthest = Math.max(furthest, rightMostIndex);
             }
+            // to find update range for next jump
             left = right + 1;
-            right = farthestPos;
-            steps += 1;
+            right = furthest;
+            // increase jump
+            jumpStep++;
         }
-        return steps;
+        // return jump
+        return jumpStep;
+    }
+    public int jumpWithRangeOptimal(int[] nums) {
+        int currFurthest = 0;
+        int nextFurthest = 0;
+        int jumpStep = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            nextFurthest = Math.max(nextFurthest, i + nums[i]);
+
+            if (i == currFurthest) {
+                jumpStep++;
+                currFurthest = nextFurthest;
+            }
+        }
+
+        return jumpStep;
     }
 
     public static void main(String[] args) {
         int[] nums = {2, 3, 1, 1, 4};
-        System.out.println(minJump(nums));
+        JumpGameII jumpGameII = new JumpGameII();
+        System.out.println(jumpGameII.jumpWithRange(nums));
     }
 }
--- JavaSolution/src/PlayGround.java
+++ JavaSolution/src/PlayGround.java
@@ -2,35 +2,92 @@ import javax.swing.*;
 import java.util.*;
 
 public class PlayGround {
-    public int maxProfit(int[] prices) {
-        // global variable
-        int totalProfit = 0;
-        // left, right
-        int left = 0;
-        int right = 1;
-        // go though arrays
-        while (right < prices.length) {
-            // left >= right; update window and do nothing
-            if (prices[left] >= prices[right]) {
-                left++;
-                right++;
-            } else {
-                if (right == prices.length - 1 || prices[right] >= prices[right + 1]) {
-                    totalProfit += prices[right] - prices[left];
-                    left = right + 1;
-                    right = left + 1;
-                } else {
-                    right++;
-                }
+    public int jumpWithDfs(int[] nums) {
+        int[] memo = new int[nums.length];
+        Arrays.fill(memo, -1);
+        return dfs(0, nums, memo);
+    }
+    private int dfs(int currPosition, int[] nums, int[] memo) {
+        // base case
+        if (currPosition == nums.length - 1) {
+            return 0;
+        }
+        if (memo[currPosition] != -1) {
+            return memo[currPosition];
+        }
+
+        // recursive case
+        int maxStep = Math.min(nums[currPosition], nums.length - 1 - currPosition);
+        int minStepToDst = Integer.MAX_VALUE;
+        for (int step = 1; step <= maxStep; step++) {
+            minStepToDst = Math.min(dfs(currPosition + step, nums, memo), minStepToDst);
+        }
+
+        int result = minStepToDst == Integer.MAX_VALUE ? minStepToDst : minStepToDst + 1;
+        memo[currPosition] = result;
+
+        return result;
+    }
+    public int jumpWithDp(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[nums.length - 1] = 0;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            int maxStep = Math.min(nums[i], nums.length - 1 - i);
+            int min = Integer.MAX_VALUE;
+            for (int step = 1; step <= maxStep; step++) {
+                min = Math.min(min, dp[i + step]);
+            }
+            if (min < Integer.MAX_VALUE) {
+                dp[i] = min + 1;
             }
         }
 
-        return totalProfit;
+        return dp[0];
+    }
+    public int jumpWithRange(int[] nums) {
+        // define a jump range
+        int jumpStep = 0;
+        int left = 0;
+        int right = 0;
+        // while the range cannot get to the dst
+        while (right < nums.length - 1) {
+            // go through curr range
+            int furthest = right + 1;
+            for (int i = left; i <= right; i++) {
+                int rightMostIndex = Math.min(i + nums[i], nums.length - 1);
+                furthest = Math.max(furthest, rightMostIndex);
+            }
+            // to find update range for next jump
+            left = right + 1;
+            right = furthest;
+            // increase jump
+            jumpStep++;
+        }
+        // return jump
+        return jumpStep;
+    }
+    public int jumpWithRangeOptimal(int[] nums) {
+        int currFurthest = 0;
+        int nextFurthest = 0;
+        int jumpStep = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            nextFurthest = Math.max(nextFurthest, i + nums[i]);
+
+            if (i == currFurthest) {
+                jumpStep++;
+                currFurthest = nextFurthest;
+            }
+        }
+
+        return jumpStep;
     }
 
     public static void main(String[] args) {
-        int[] nums = {7,1,5,3,6,4};
+        int[] nums = {3,0,0,1,4};
         PlayGround playGround = new PlayGround();
-        System.out.println(playGround.maxProfit(nums));
+        System.out.println(playGround.jumpWithDp(nums));
     }
 }
