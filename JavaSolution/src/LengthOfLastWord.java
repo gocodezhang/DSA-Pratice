fd3f247ae53b15b66ab9011f37403342e9df72ff
Gaoyuan Zhang <zgy25483387@gmail.com>
2024-12-15 11:41:51 -0800

    lc 12/15

--- JavaSolution/src/LengthOfLastWord.java
+++ JavaSolution/src/LengthOfLastWord.java
@@ -24,6 +24,29 @@ public class LengthOfLastWord {
         // return lastLetter - afterSpace
         return lastLetter - afterSpace;
     }
+    public int lengthOfLastWord(String s) {
+        // search from the back
+        // seenWord
+        boolean seenWord = false;
+        int prevSpaceIndex = s.length();
+        int index = s.length() - 1;
+
+        while (index >= 0) {
+            char currChar = s.charAt(index);
+            if (currChar != ' ') {
+                seenWord = true;
+            } else {
+                if (seenWord) {
+                    break;
+                }
+                prevSpaceIndex = index;
+            }
+            index--;
+        }
+
+        return prevSpaceIndex - index - 1;
+
+    }
     public static void main(String[] args) {
         String s = "   fly me   to   the moon  ";
         System.out.println(lenOfLastWord(s));
--- JavaSolution/src/PlayGround.java
+++ JavaSolution/src/PlayGround.java
@@ -2,53 +2,34 @@ import javax.swing.*;
 import java.util.*;
 
 public class PlayGround {
-    static HashMap<Integer, Character> map = new HashMap<>();
-    static {map.put(1, 'I');
-    map.put(5, 'V');
-    map.put(10, 'X');
-    map.put(50, 'L');
-    map.put(100, 'C');
-    map.put(500, 'D');
-    map.put(1000, 'M');}
 
-    public String intToRoman(int num) {
-        // put digits into stack
-        Stack<Integer> stack = new Stack<>();
-        while (num != 0) {
-            int digit = num % 10;
-            stack.push(digit);
-            num = num / 10;
-        }
+    public int lengthOfLastWord(String s) {
+        // search from the back
+        // seenWord
+        boolean seenWord = false;
+        int prevSpaceIndex = s.length();
+        int index = s.length() - 1;
 
-        StringBuilder builder = new StringBuilder();
-        // process the digits from the top
-        while (!stack.isEmpty()) {
-            int currDigit = stack.pop();
-            int unit = (int) Math.pow(10, stack.size());
-            // check if digit is 4 or 9
-            if (currDigit == 4 || currDigit == 9) {
-                // when true, upscale it and substract
-                currDigit = currDigit + 1;
-                builder.append(map.get(unit));
-                builder.append(map.get(currDigit * unit));
+        while (index >= 0) {
+            char currChar = s.charAt(index);
+            if (currChar != ' ') {
+                seenWord = true;
             } else {
-                // when false, find corresponding symbol
-                if (currDigit >= 5) {
-                    currDigit = currDigit - 5;
-                    builder.append(map.get(5 * unit));
+                if (seenWord) {
+                    break;
                 }
-                while (currDigit > 0) {
-                    builder.append(map.get(unit));
-                    currDigit--;
-                }
+                prevSpaceIndex = index;
             }
+            index--;
         }
-        return builder.toString();
+
+        return prevSpaceIndex - index - 1;
+
     }
 
 
     public static void main(String[] args) {
         PlayGround playground = new PlayGround();
-        System.out.println(playground.intToRoman(1994));
+        System.out.println(playground.lengthOfLastWord("   fly me   to   the moon  "));
     }
 }
