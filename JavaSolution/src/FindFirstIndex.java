c6354ec4ff2586245ec3e61d162792966b21c6c9
Gaoyuan Zhang <zgy25483387@gmail.com>
2025-01-02 08:19:15 +0800

    lc 1/1

--- JavaSolution/src/FindFirstIndex.java
+++ JavaSolution/src/FindFirstIndex.java
@@ -2,4 +2,28 @@ public class FindFirstIndex {
     public int strStr(String haystack, String needle) {
         return haystack.indexOf(needle);
     }
+    public int strStrWOBuiltIn(String haystack, String needle) {
+        int length = needle.length();
+        char starting = needle.charAt(0);
+
+        for (int i = 0; i < haystack.length(); i++) {
+            if (starting == haystack.charAt(i)) {
+                int count = 1;
+                boolean match = true;
+
+                while (match && count < length && i + count < haystack.length()) {
+                    if (needle.charAt(count) != haystack.charAt(count + i)) {
+                        match = false;
+                    } else {
+                        count++;
+                    }
+                }
+                if (match && count == length) {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
 }
--- JavaSolution/src/PlayGround.java
+++ JavaSolution/src/PlayGround.java
@@ -2,71 +2,35 @@ import javax.swing.*;
 import java.util.*;
 
 public class PlayGround {
+    public int strStr(String haystack, String needle) {
+        int length = needle.length();
+        char starting = needle.charAt(0);
 
-    public String convert(String s, int numRows) {
-        if (numRows == 1) {
-            return s;
-        }
-        List<List<Character>> rows = new ArrayList<>();
-        for (int i = 0; i < numRows; i++) {
-            rows.add(new ArrayList<>());
-        }
+        for (int i = 0; i < haystack.length(); i++) {
+            if (starting == haystack.charAt(i)) {
+                int count = 1;
+                boolean match = true;
 
-        int range = numRows * 2 - 2;
-        int currIndex = 0;
-
-        while (currIndex < s.length()) {
-            int step = 0;
-            while (step < range && currIndex + step < s.length()) {
-                char curr = s.charAt(currIndex + step);
-                int currRow = step < numRows ? step : (range - step);
-                List<Character> row = rows.get(currRow);
-                row.add(curr);
-                step++;
-            }
-            currIndex += range;
-        }
-        StringBuilder builder = new StringBuilder();
-
-        for (int i = 0; i < rows.size(); i++) {
-            List<Character> row = rows.get(i);
-            for (int j = 0; j < row.size(); j++) {
-                builder.append(row.get(j));
-            }
-        }
-
-        return builder.toString();
-    }
-    public String convertConstantMemory(String s, int numRows) {
-        if (numRows == 1) {
-            return s;
-        }
-        int sessionLength = 2 * numRows - 2;
-
-        StringBuilder builder = new StringBuilder();
-
-        for (int row = 0; row < numRows; row++) {
-            int firstIndex = row;
-            while (firstIndex < s.length()) {
-//                System.out.println("row: " + row);
-//                System.out.println("char: " + s.charAt(firstIndex));
-                builder.append(s.charAt(firstIndex));
-                int secondIndex = firstIndex + (sessionLength - 2 * row);
-                if (row != 0 && row != numRows - 1 && secondIndex < s.length()) {
-                    builder.append(s.charAt(secondIndex));
+                while (match && count < length && i + count < haystack.length()) {
+                    if (needle.charAt(count) != haystack.charAt(count + i)) {
+                        match = false;
+                    } else {
+                        count++;
+                    }
                 }
-
-                firstIndex += sessionLength;
+                if (match && count == length) {
+                    return i;
+                }
             }
         }
 
-        return builder.toString();
+        return -1;
     }
 
 
 
     public static void main(String[] args) {
         PlayGround playground = new PlayGround();
-        System.out.println(playground.convertConstantMemory("PAYPALISHIRING",3));
+        System.out.println(playground.strStr("sadbutsad","sad"));
     }
 }
