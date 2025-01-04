cc6cf39ab9e5b5e61c7169d211b479c505d173e7
Gaoyuan Zhang <zgy25483387@gmail.com>
2025-01-04 08:06:04 +0800

    1/3

--- JavaSolution/src/PlayGround.java
+++ JavaSolution/src/PlayGround.java
@@ -2,35 +2,88 @@ import javax.swing.*;
 import java.util.*;
 
 public class PlayGround {
-    public int strStr(String haystack, String needle) {
-        int length = needle.length();
-        char starting = needle.charAt(0);
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> result = new ArrayList<>();
+        List<String> currLine = new ArrayList<>();
+        int currLineLen = words[0].length() + 1;
+        currLine.add(words[0]);
+        // go through words
+        for (int i = 1; i < words.length; i++) {
+            // track currLineLength
+            String word = words[i];
+            if (currLineLen + word.length() <= maxWidth) {
+                // if currLine can fit curr word, add
+                currLine.add(word);
+                currLineLen += word.length() + 1;
+//                System.out.println(currLineLen);
+//                System.out.println(currLine);
+            } else {
+                // if no, format and add into result
+                result.add(formatLine(currLine, maxWidth, false));
 
-        for (int i = 0; i < haystack.length(); i++) {
-            if (starting == haystack.charAt(i)) {
-                int count = 1;
-                boolean match = true;
+                currLine.clear();
+                currLine.add(word);
+                currLineLen = word.length() + 1;
+            }
+        }
 
-                while (match && count < length && i + count < haystack.length()) {
-                    if (needle.charAt(count) != haystack.charAt(count + i)) {
-                        match = false;
-                    } else {
-                        count++;
-                    }
-                }
-                if (match && count == length) {
-                    return i;
+        // address last line
+        String lastLine = formatLine(currLine, maxWidth, true);
+
+        result.add(lastLine);
+
+        // return
+        return result;
+    }
+    public String formatLine(List<String> wordsInLine, int maxWidth, boolean last) {
+        // find out the remaining spaces and slots
+        // System.out.println(wordsInLine);
+        int wordsLen = 0;
+
+        for (int i = 0; i < wordsInLine.size(); i++) {
+            wordsLen += wordsInLine.get(i).length();
+        }
+
+        // case 1 - one word or last line
+        if (wordsInLine.size() == 1 || last) {
+            String text = String.join(" ", wordsInLine);
+
+            // add space padding
+            return text + " ".repeat(maxWidth - text.length());
+        }
+
+        // case 2 - 2 or more words in the line
+        int slots = wordsInLine.size() - 1;
+        // find out base + extra
+        int base = (maxWidth - wordsLen) / slots;
+        int extra = (maxWidth - wordsLen) - base * slots;
+        // System.out.println(extra);
+        // build the line and assign extra 1 by 1 from left
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < wordsInLine.size(); i++) {
+            String word = wordsInLine.get(i);
+            builder.append(word);
+
+            if (i != wordsInLine.size() - 1) {
+                builder.append(" ".repeat(base));
+
+                if (extra > 0) {
+                    builder.append(" ");
+                    extra--;
                 }
             }
         }
 
-        return -1;
+        return builder.toString();
     }
 
 
 
     public static void main(String[] args) {
         PlayGround playground = new PlayGround();
-        System.out.println(playground.strStr("sadbutsad","sad"));
+        String[] words = {"Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"};
+        System.out.println(playground.fullJustify(words, 20));
     }
+
 }
--- JavaSolution/src/TextJustification.java
+++ JavaSolution/src/TextJustification.java
@@ -82,6 +82,81 @@ public class TextJustification {
         }
         return result + space;
     }
+    public List<String> fullJustifySecondSolution(String[] words, int maxWidth) {
+        List<String> result = new ArrayList<>();
+        List<String> currLine = new ArrayList<>();
+        int currLineLen = words[0].length() + 1;
+        currLine.add(words[0]);
+        // go through words
+        for (int i = 1; i < words.length; i++) {
+            // track currLineLength
+            String word = words[i];
+            if (currLineLen + word.length() <= maxWidth) {
+                // if currLine can fit curr word, add
+                currLine.add(word);
+                currLineLen += word.length() + 1;
+//                System.out.println(currLineLen);
+//                System.out.println(currLine);
+            } else {
+                // if no, format and add into result
+                result.add(formatLine(currLine, maxWidth, false));
+
+                currLine.clear();
+                currLine.add(word);
+                currLineLen = word.length() + 1;
+            }
+        }
+
+        // address last line
+        String lastLine = formatLine(currLine, maxWidth, true);
+
+        result.add(lastLine);
+
+        // return
+        return result;
+    }
+    public String formatLine(List<String> wordsInLine, int maxWidth, boolean last) {
+        // find out the remaining spaces and slots
+        // System.out.println(wordsInLine);
+        int wordsLen = 0;
+
+        for (int i = 0; i < wordsInLine.size(); i++) {
+            wordsLen += wordsInLine.get(i).length();
+        }
+
+        // case 1 - one word or last line
+        if (wordsInLine.size() == 1 || last) {
+            String text = String.join(" ", wordsInLine);
+
+            // add space padding
+            return text + " ".repeat(maxWidth - text.length());
+        }
+
+        // case 2 - 2 or more words in the line
+        int slots = wordsInLine.size() - 1;
+        // find out base + extra
+        int base = (maxWidth - wordsLen) / slots;
+        int extra = (maxWidth - wordsLen) - base * slots;
+        // System.out.println(extra);
+        // build the line and assign extra 1 by 1 from left
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < wordsInLine.size(); i++) {
+            String word = wordsInLine.get(i);
+            builder.append(word);
+
+            if (i != wordsInLine.size() - 1) {
+                builder.append(" ".repeat(base));
+
+                if (extra > 0) {
+                    builder.append(" ");
+                    extra--;
+                }
+            }
+        }
+
+        return builder.toString();
+    }
     public static void main(String[] args) {
         String[] words = {"Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"};
         TextJustification textJustification = new TextJustification();
